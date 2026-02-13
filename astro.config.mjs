8e0b2c3b83bc86b7eb843b6460415882ff9efa50
Pablo Alejos <pabloalejos@Pablos-MacBook-Pro-2.local>
2026-02-13 13:33:40 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://PabloAlejos.github.io',
-  base: '/fork-and-commit', // Solo si es un repo de proyecto, no si es tu-usuario.github.io
+  base: '/fork-and-commit',
+  output: 'static',
 });
